36aabbcc2e9f1896d50dc4d14e3b02e1d2b60bd0
Alexandra <awinterdrova@gmail.com>
2023-08-05 23:08:38 -0600

    added database functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const textDB = await openDB('jate', 1);
+  const yz = textDB.transaction('jate', 'readwrite');
+  const store = yz.objectStore('jate');
+  const request = store.put({ jate: content});
+  const result = await request;
+  console.log("This stuff worked", result);
+};
 
 // DONE? Add logic for a method that gets all the content from the database
 export const getDb = async () => {
